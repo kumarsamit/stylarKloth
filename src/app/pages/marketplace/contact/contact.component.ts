0710a654ecd48b58da51fcfddef09402e4952fcc
Amit Kumar <kumarsamit335@gmail.com>
2024-12-18 22:00:03 +0530

    contact and about

--- src/app/pages/marketplace/contact/contact.component.ts
+++ src/app/pages/marketplace/contact/contact.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+	selector: 'app-contact',
+	templateUrl: './contact.component.html',
+	styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+
+	contactFormGroup = new FormGroup({
+		name: new FormControl('', [Validators.required]),
+		email: new FormControl('', [Validators.required, Validators.email]),
+		phone: new FormControl('', [Validators.required]),
+		desc: new FormControl('', [Validators.required]),
+	})
+
+	submitQuery() {
+		console.log("!11111111", this.contactFormGroup.value)
+	}
 
 }
--- src/app/pages/marketplace/contact/contact.module.ts
+++ src/app/pages/marketplace/contact/contact.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact.component';
 import { Routes, RouterModule } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule } from '@angular/material/input';
 
 const routes: Routes = [
   {
@@ -16,8 +19,11 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
     RouterModule.forChild(routes)
-
   ]
 })
 export class ContactModule { }
