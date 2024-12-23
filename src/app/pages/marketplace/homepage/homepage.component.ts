d0930deec457bb1c7ab9c2af34753e0d3b6f13f5
Amit Kumar <kumarsamit335@gmail.com>
2024-12-23 23:13:01 +0530

    share button

--- src/app/pages/marketplace/homepage/homepage.component.ts
+++ src/app/pages/marketplace/homepage/homepage.component.ts
@@ -1,85 +1,95 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+	selector: 'app-homepage',
+	templateUrl: './homepage.component.html',
+	styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent {
 
-  products:any=[
-    {
-      name : "Blue dress",
-      price : "400",
-      offerPrice : "600",
-      img : "./assets/images/dummy/child-1.jpg"
-    },
-    {
-      name : "Blue dress",
-      price : "400",
-      // offerPrice : "600",
-      img : "./assets/images/dummy/child-1.jpg"
-    },
-    {
-      name : "Blue dress",
-      price : "400",
-      offerPrice : "600",
-      img : "./assets/images/dummy/child-1.jpg"
-    },
-    {
-      name : "Blue dress",
-      price : "400",
-      offerPrice : "600",
-      img : "./assets/images/dummy/child-1.jpg"
-    },
-    {
-      name : "Blue dress",
-      price : "400",
-      offerPrice : "600",
-      img : "./assets/images/dummy/child-1.jpg"
-    },
-    {
-      name : "Blue dress",
-      price : "400",
-      offerPrice : "600",
-      img : "./assets/images/dummy/child-1.jpg"
-    },
-    {
-      name : "Blue dress",
-      price : "400",
-      offerPrice : "600",
-      img : "./assets/images/dummy/child-1.jpg"
-    },
-    {
-      name : "Blue dress",
-      price : "400",
-      offerPrice : "600",
-      img : "./assets/images/dummy/child-1.jpg"
-    },
-    {
-      name : "Blue dress",
-      price : "400",
-      // offerPrice : "600",
-      img : "./assets/images/dummy/child-1.jpg"
-    },
-    {
-      name : "Blue dress",
-      price : "400",
-      offerPrice : "600",
-      img : "./assets/images/dummy/child-1.jpg"
-    },
-    {
-      name : "Blue dress",
-      price : "400",
-      offerPrice : "600",
-      img : "./assets/images/dummy/child-1.jpg"
-    },
-    {
-      name : "Blue dress",
-      price : "400",
-      offerPrice : "600",
-      img : "./assets/images/dummy/child-1.jpg"
-    }
-  ]
+	products: any = [
+		{
+			name: "Blue dress",
+			price: "400",
+			offerPrice: "600",
+			img: "./assets/images/dummy/child-1.jpg"
+		},
+		{
+			name: "Blue dress",
+			price: "400",
+			// offerPrice : "600",
+			img: "./assets/images/dummy/child-1.jpg"
+		},
+		{
+			name: "Blue dress",
+			price: "400",
+			offerPrice: "600",
+			img: "./assets/images/dummy/child-1.jpg"
+		},
+		{
+			name: "Blue dress",
+			price: "400",
+			offerPrice: "600",
+			img: "./assets/images/dummy/child-1.jpg"
+		},
+		{
+			name: "Blue dress",
+			price: "400",
+			offerPrice: "600",
+			img: "./assets/images/dummy/child-1.jpg"
+		},
+		{
+			name: "Blue dress",
+			price: "400",
+			offerPrice: "600",
+			img: "./assets/images/dummy/child-1.jpg"
+		},
+		{
+			name: "Blue dress",
+			price: "400",
+			offerPrice: "600",
+			img: "./assets/images/dummy/child-1.jpg"
+		},
+		{
+			name: "Blue dress",
+			price: "400",
+			offerPrice: "600",
+			img: "./assets/images/dummy/child-1.jpg"
+		},
+		{
+			name: "Blue dress",
+			price: "400",
+			// offerPrice : "600",
+			img: "./assets/images/dummy/child-1.jpg"
+		},
+		{
+			name: "Blue dress",
+			price: "400",
+			offerPrice: "600",
+			img: "./assets/images/dummy/child-1.jpg"
+		},
+		{
+			name: "Blue dress",
+			price: "400",
+			offerPrice: "600",
+			img: "./assets/images/dummy/child-1.jpg"
+		},
+		{
+			name: "Blue dress",
+			price: "400",
+			offerPrice: "600",
+			img: "./assets/images/dummy/child-1.jpg"
+		}
+	]
+
+
+	shareOnWhatsApp() {
+		let referralCode = 'ABC123';
+		let referralMessage = `Use code ${referralCode} to get 20% off your next order! Your friend gets 10% off their first purchase. Start here: `;
+		let referralLink = "https://www.stylarkloth.com";
+		const message = `${referralMessage} ${referralLink}`;
+		const whatsappURL = `https://wa.me/?text=${encodeURIComponent(message)}`;
+		window.open(whatsappURL, '_blank');
+	}
 
 }
