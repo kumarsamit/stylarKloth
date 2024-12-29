23866cd0e4be0860d3c60e14ec10ffcba879442d
Amit Kumar <kumarsamit335@gmail.com>
2024-12-29 16:32:45 +0530

    admin

--- src/app/pages/marketplace/user-profile/user-profile.component.ts
+++ src/app/pages/marketplace/user-profile/user-profile.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss']
+	selector: 'app-user-profile',
+	templateUrl: './user-profile.component.html',
+	styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent {
+
+	isEditable: boolean = false;
+
+	userFormGroup = new FormGroup({
+		name: new FormControl('', [Validators.required]),
+		email: new FormControl('', [Validators.required, Validators.email]),
+		phone: new FormControl('', [Validators.required]),
+		sex: new FormControl('', [Validators.required]),
+	})
+
+	updateUserDetails() {
+		console.log("!11111111", this.userFormGroup.value)
+	}
+
+	editForm() {
+		this.isEditable = !this.isEditable;
+	}
+
+
+
 
 }
--- src/app/pages/marketplace/user-profile/user-profile.module.ts
+++ src/app/pages/marketplace/user-profile/user-profile.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { UserProfileComponent } from './user-profile.component';
 import { RouterModule, Routes } from '@angular/router';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule } from '@angular/material/input';
 
 
 const routes: Routes = [
@@ -19,6 +22,10 @@ const routes: Routes = [
   imports: [
     CommonModule,
     MatExpansionModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
     RouterModule.forChild(routes)
   ]
 })
